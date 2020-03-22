8109f7dba76b8d39d14fa692912eb1eb92292fea
hikanashi <hikanashi@gmail.com>
2020-03-22 23:21:17 +0900

    Added environment variable settings for authentication proxy

--- scripts/vcpkg_package.bat
+++ scripts/vcpkg_package.bat
@@ -1,4 +1,18 @@
 @echo off
 
+set PROXYUSER=%USERNAME%
+
+set PROXYPASS=
+set /P PROXYPASS="Input User(%PROXYUSER%)'s password: "
+
+set PROXYSHEMA=http://
+set PROXYAUTHORITY=127.0.0.1:8888
+
+set HTTP_PROXY=%PROXYSHEMA%%PROXYUSER%:%PROXYPASS%@%PROXYAUTHORITY%
+set HTTPS_PROXY=%PROXYSHEMA%%PROXYUSER%:%PROXYPASS%@%PROXYAUTHORITY%
+rem echo set HTTPS_PROXY=%HTTPS_PROXY%
+
 cd vcpkg
-vcpkg install openssl curl jansson libevent protobuf grpc
+vcpkg install openssl nghttp2 curl jansson libevent protobuf grpc
+
+pause
