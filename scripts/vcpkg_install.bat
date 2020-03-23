a48a82747772b7e72785e68cab1da106e5a36503
hikanashi <hikanashi@gmail.com>
2020-03-23 21:00:08 +0900

    Merged repository clone and bootstrap execution and package installation.

--- scripts/vcpkg_install.bat
+++ scripts/vcpkg_install.bat
@@ -1,9 +1,31 @@
 @echo off
 
-rem git clone https://github.com/Microsoft/vcpkg.git
-git clone https://github.com/hikanashi/vcpkg.git
+set PROXYUSER=%USERNAME%
+
+set PROXYPASS=
+set /P PROXYPASS="Input User(%PROXYUSER%)'s password: "
+
+set PROXYSHEMA=http://
+set PROXYAUTHORITY=127.0.0.1:8888
+
+set HTTP_PROXY=%PROXYSHEMA%%PROXYUSER%:%PROXYPASS%@%PROXYAUTHORITY%
+set HTTPS_PROXY=%PROXYSHEMA%%PROXYUSER%:%PROXYPASS%@%PROXYAUTHORITY%
+rem echo set HTTPS_PROXY=%HTTPS_PROXY%
+
+# clone vcpkg
+if not exist vcpkg (
+	REM git clone https://github.com/Microsoft/vcpkg.git
+	git clone https://github.com/hikanashi/vcpkg.git
+)
 
 cd vcpkg
-powershell -Command ".\bootstrap-vcpkg.bat"
-REM powershell -Command ".\vcpkg integrate install"
+
+
+if not exist vcpkg.exe (
+	powershell -Command ".\bootstrap-vcpkg.bat"
+	REM powershell -Command ".\vcpkg integrate install"
+)
+
+vcpkg install pthread openssl nghttp2 curl jansson libevent protobuf grpc
+
 pause
